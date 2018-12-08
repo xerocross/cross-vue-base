ecd857ddb521e85fac6b71fb8aa990710379b2f0
xerocross <adam.cross.inc@gmail.com>
2018-12-07 19:08:48 -0500

    corrections

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import TextLoader from "./components/text-loader.vue";
 import SimpleQajax from "./helpers/simple-qajax.js";
+import QueryParams from "./helpers/query-params.js";
 
 
-export { TextLoader , SimpleQajax};
+export { TextLoader , SimpleQajax, QueryParams};
