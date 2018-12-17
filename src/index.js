0799b8500e30f6c147d62318692042ad76ff4be9
xerocross <adam.cross.inc@gmail.com>
2018-12-17 12:51:09 -0500

    add new component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import TextLoader from "./components/text-loader.vue";
 import SimpleQajax from "./helpers/simple-qajax.js";
 import QueryParams from "./helpers/query-params.js";
+import DemoLayout from "./components/demo-layout.vue";
 
 
-export { TextLoader , SimpleQajax, QueryParams};
+export { TextLoader , SimpleQajax, QueryParams, DemoLayout};
