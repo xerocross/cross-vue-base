02854f08c2acf7d163753567a88f5a29cbfa621a
xerocross <adam.cross.inc@gmail.com>
2019-01-09 15:33:27 -0500

    add more helpers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import SimpleQajax from "./helpers/simple-qajax.js";
 import QueryParams from "./helpers/query-params.js";
 import DemoLayout from "./components/demo-layout.vue";
 import DrawerDiv from "./components/drawer-div.vue";
+import StoreLocal from "./helpers/store-local.js";
 
 
-export { TextLoader , SimpleQajax, QueryParams, DemoLayout, DrawerDiv};
+export { TextLoader , SimpleQajax, QueryParams, DemoLayout, DrawerDiv, StoreLocal};
