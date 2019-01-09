2c90793a13de121b19e6c133cbf2579b48663089
xerocross <nonya@business.com>
2019-01-09 12:46:05 -0800

    add new index items

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import QueryParams from "./helpers/query-params.js";
 import DemoLayout from "./components/demo-layout.vue";
 import DrawerDiv from "./components/drawer-div.vue";
 import StoreLocal from "./helpers/store-local.js";
+import SimpleHashMap from "./helpers/simple-hash-map.js";
+import SimpleStringSet from "./helpers/simple-string-set.js";
+import ArrayHelpers from "./helpers/array-helpers.js";
 
 
-export { TextLoader , SimpleQajax, QueryParams, DemoLayout, DrawerDiv, StoreLocal};
+export { TextLoader , SimpleQajax, QueryParams, DemoLayout, DrawerDiv,
+  StoreLocal, SimpleHashMap, SimpleStringSet, ArrayHelpers
+};
